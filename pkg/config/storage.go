1510229821be9d08a45ed1d50220f719e3332cde
Guillaume Rivest <turns.coffee.into.scripts@gmail.com>
2023-04-17 14:08:36 -0400

    Manually commit of some changes because github PAT is bullshit

--- pkg/config/storage.go
+++ pkg/config/storage.go
@@ -1,7 +1,21 @@
 package config
 
-import "turnscoffeeintoscripts/am/pkg/model"
+import (
+	"turnscoffeeintoscripts/am/pkg/model"
+	"turnscoffeeintoscripts/am/pkg/terminal"
+
+	"gopkg.in/yaml.v3"
+)
 
 type Storage struct {
 	Aliases []model.Alias
+}
+
+func (s *Storage) Marshal() []byte {
+	if yd, err := yaml.Marshal(&s); err != nil {
+		terminal.ErrorMessage("Failed to marshal 'Storage' struct")
+		return nil
+	} else {
+		return yd
+	}
 }
